7eb69e64a7694b68aaa6a63145cf724ff3a40c7b
Luc Tielen <luc.tielen@gmail.com>
2022-11-18 12:01:47 +0100

    Add prettier to setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import { promisify } from 'util';
-import fs from "fs/promises"
-import { file, withFile } from "tmp-promise"
-import process from 'child_process'
-import express from "express"
+import fs from 'fs/promises';
+import { file, withFile } from 'tmp-promise';
+import process from 'child_process';
+import express from 'express';
 import bodyParser from 'body-parser';
 
 const execFile = promisify(process.execFile);
 
-const wallocObjFile = './walloc.o'
+const wallocObjFile = './walloc.o';
 
 const compileEclairProgram = async (program) =>
   withFile(async ({ path: inputFile }) => {
     const { path: outputFile, cleanup } = await file();
     await fs.writeFile(inputFile, program);
-    const { stderr } = await execFile('./compile_eclair.sh', [inputFile, wallocObjFile, outputFile]);
+    const { stderr } = await execFile('./compile_eclair.sh', [
+      inputFile,
+      wallocObjFile,
+      outputFile,
+    ]);
 
     if (stderr) {
       return { errorMessage: stderr };
@@ -23,7 +27,7 @@ const compileEclairProgram = async (program) =>
   });
 
 const logger = (req, res, next) => {
-  res.on("finish", () => {
+  res.on('finish', () => {
     console.log(res.statusCode, JSON.stringify(req.body || {}));
   });
   next();
@@ -38,12 +42,14 @@ const main = () => {
 
   app.post('/', async (req, res) => {
     const program = req.body?.program;
-    if (!program || typeof program !== "string") {
-      res.status(400).send("Bad request");
+    if (!program || typeof program !== 'string') {
+      res.status(400).send('Bad request');
       return;
     }
 
-    const { errorMessage, wasmFile, cleanup } = await compileEclairProgram(program);
+    const { errorMessage, wasmFile, cleanup } = await compileEclairProgram(
+      program
+    );
     if (errorMessage) {
       res.status(404).send(errorMessage);
       return;
@@ -51,9 +57,9 @@ const main = () => {
 
     res.download(wasmFile);
     await cleanup();
-  })
+  });
 
   app.listen(8080);
-}
+};
 
-main()
+main();
