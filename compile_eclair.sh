36b948f7691bd8d966ec87da1ef0a174e779da11
Luc Tielen <luc.tielen@gmail.com>
2022-11-21 10:39:17 +0100

    Turn off colorized compiler output

--- compile_eclair.sh
+++ compile_eclair.sh
@@ -8,7 +8,7 @@ OUTPUT_FILE=$3
 LL_FILE=$INPUT_FILE.ll
 OBJ_FILE=$INPUT_FILE.o
 
-eclair compile --target wasm32 $INPUT_FILE > $LL_FILE
+ECLAIR_USE_COLOR=0 eclair compile --target wasm32 $INPUT_FILE > $LL_FILE
 clang-14 -Oz --target=wasm32 -mbulk-memory -nostdlib -Wno-override-module -c -o $OBJ_FILE $LL_FILE
 wasm-ld-14 --no-entry --import-memory -o $OUTPUT_FILE $OBJ_FILE $WALLOC_OBJ_FILE
 
